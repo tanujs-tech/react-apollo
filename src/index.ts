bbbd066d65b150e4f0f05dc0333f404ad086d44e
Victor Vlasenko <victor.vlasenko@sysgears.com>
2016-10-26 19:23:26 +0300

    Disable lint error

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import ApolloProvider from './ApolloProvider';
 import graphql, { withApollo } from './graphql';
 
 // expose easy way to join queries from recompose
-const compose = require('recompose').compose;
+const compose = require('recompose').compose; // tslint:disable-line
 
 export { ApolloProvider, graphql, withApollo, compose };
